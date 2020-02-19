dee1b07fd578bde887d7342ce27c36bee45da5ac
numanab44 <numan.cs12@gmail.com>
2020-02-20 04:14:24 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(int soundNumber, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.blue),
+              buildKey(3, Colors.purple),
+              buildKey(4, Colors.teal),
+              buildKey(5, Colors.yellow),
+              buildKey(6, Colors.deepOrange),
+              buildKey(7, Colors.pink),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
